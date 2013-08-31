aaaa56f6ca0d03b8c883bf9332dcc571a5106cdf
Tiff Ting <tiff@bazaarvoice.com>
2013-08-31 17:12:10 -0500

    Updated Local Forager Contest page for post-2013 festival

--- local_forager_contest.php
+++ local_forager_contest.php
@@ -3,17 +3,33 @@
   $page_title = 'Local Forager Contest';
   require 'inc/top.inc.php';
 ?>
-<style type="text/css" media="screen">
-  .salesforce_logo{float:right; margin-top:20px; padding-bottom:20px;}
-</style>
+
   <section class="kraft">
     <div class="inner">
-      <h1>Local Forager Contest</h1>
-      <style type="text/css" media="screen">
-        iframe{width:100%; height:900px; border:none;}
-      </style>
-      <iframe src="http://lacocinasf.force.com/sfsff" allowtransparency /></iframe>
-      <div class="salesforce_logo"><a href="http://salesforce.com"><img src="http://lacocinasf.force.com/sfsff/resource/1305497954000/poweredbysfdc" /></a></div>
+
+      <h1>Local forager contest</h1>
+
+      <p><img src="https://s3.amazonaws.com/sfsff/whole_foods_round.png" class="pull-left"/><a href="http://www.wholefoodsmarket.com/">Whole Foods Market</a> and <a href="http://www.lacocinasf.org/">La Cocina</a> share a commitment to finding the best and the brightest of local cooks who are ready to formalize their business, and what better place to launch than the Street Food Festival? We&rsquo;re always searching, together, high and low for those folks who are looking to take their next food step: informal vendors, launching food trucks, owner-operated, chef-driven restaurants, your next door neighbor or anyone whose food you think is the bee&rsquo;s knees. These are the folks we want at the Street Food Festival.</p>
+
+      <p>The 2013 Local Forager contest with Whole Foods Market has come to an end.  After an intense round of voting, winners won free booth space at the festival, logistical support for the festival from La Cocina staff, and an opportunity to meet the marketing team behind Whole Foods Market and of course, Whole Foods Market&rsquo;s <a href="https://www.facebook.com/LOCALFORAGER">Local Forager Harv Singh</a>.</p>
+
+      <p>Congratulations to the winners!</p>
+      <p>
+        <img src="/z_photos/2013/local_forager_winners.jpg" alt="2013 Local Forager Contest winners" width="535" height="343"/>
+        <small class="caption">Photo credit to Jeff Spirer</small>
+      </p>
+
+      <ul class="text">
+        <li><a href="http://www.clearlykombucha.com/">Clearly Kombucha</a></li>
+        <li><a href="http://www.fritasshack.com/">Fritas Shack</a></li>
+        <li><a href="https://www.facebook.com/gyroandcheesesteaktrolley">Gyro &amp; Cheesesteak Trolley</a></li>
+        <li><a href="http://www.kamafoodlab.com/">Kama Food Lab</a></li>
+        <li><a href="https://www.facebook.com/KoJaKitchen">Koja Kitchen</a></li>
+      </ul>
+
+      <p>For more information on how to participate in next year&rsquo;s contest and festival, check out the Street Food Festival <a href="https://www.facebook.com/sanfranciscostreetfoodfestival">Facebook page</a> and <a href="https://twitter.com/streetfoodsf">Twitter feed</a> for continued updates.</p>
+
     </div>
   </section>
+
 <?php require 'inc/bottom.inc.php'; ?>
