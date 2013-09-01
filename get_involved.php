9b396a40164786a19201ca3ca955f9179a693337
Tiff Ting <tiff@bazaarvoice.com>
2013-08-31 20:19:00 -0500

    Updated Get Involved page for post-2013 festival

--- get_involved.php
+++ get_involved.php
@@ -6,19 +6,11 @@
   <section class="kraft">
     <div class="inner">
       <h1>Get involved</h1>
-      <p>Thank you for your interest in volunteering for the San Francisco Street Food Festival. The events for this year are over, but we will post opportunities for the next festival when they become available.</p>
-      <?php
-      /*<p>Be a volunteer for this year&rsquo;s San Francisco Street Food Festival by following the directions below:</p>
-      <ul class="text">
-        <li>2013 SF Night Market &amp; Street Food Festival: fill out <a href="https://docs.google.com/forms/d/1l1pb6I94EX9clA0M4measXjiXckjW9MEYNNoPNvOWI0/viewform?edit_requested=true">this form</a></li>
-        <li>2013 Food &amp; Entrepreneurship Conference: email <a href="mailto:julie_l_lindsey@yahoo.com">Julie Lindsey</a> for more information</li>
-        <li><a href="https://docs.google.com/spreadsheet/viewform?pli=1&formkey=dG1NR1R0al85Y1JoRVZMVVd2VXZSMkE6MQ#gid=0">Night Market General Volunteer Application</a></li>
-        <li><a href="https://docs.google.com/a/lacocinasf.org/spreadsheet/viewform?pli=1&formkey=dFBvNGtmUDVtclhlQS03NzZydXk2Q1E6MQ#gid=0">2012 Voluntarios Generales para el Festival de Comida Callejera</a></li>
-        <li><a href="https://docs.google.com/a/lacocinasf.org/spreadsheet/viewform?formkey=dHZMQWJJTVJ4M25lUm11M3p5RndCTmc6MA#gid=0">2012 SF Street Food Festival Culinary Volunteers</a></li>
-        <li><a href="https://docs.google.com/a/lacocinasf.org/spreadsheet/viewform?formkey=dGlVWE16bk5tXzllcUhxS0VnbzJISXc6MQ#gid=0">La Cocina&rsquo;s Food &amp; Entrepreneurship Conference Volunteers</a></li>
-      </ul>
-      */
-      ?>
+      <p>A huge thank you to the hundreds of volunteers who came to support the 2013 Street Food Festival.  You built seating for tens of thousands of people, shuttled food and ice across perilous crowds of people, sold an unprecedented amount of street food merchandise, and, man, do you know how to rock neon colored t-shirts.</p>
+      <p>
+        <img src="/z_photos/2013/get_involved-volunteers.jpg" alt="SFSFF volunteers" width="229" height="343"/>
+      </p>
+      <p>Thank you for your support and can&rsquo;t wait to have you at next year&rsquo;s event! Till then, check out La Cocina&rsquo;s <a href="http://lacocinasf.org/volunteer">volunteer page</a> for food and entrepreneurship opportunities year-round.</p>
     </div>
   </section>
 <?php require 'inc/bottom.inc.php'; ?>
