e9d1a05eb4e7034f6c4b4ec734ffa6d8ba0c3b61
Tiff Ting <tiff@bazaarvoice.com>
2013-09-06 23:03:46 -0500

    Added photo to Mission neighbors page

--- mission_neighbors.php
+++ mission_neighbors.php
@@ -8,6 +8,10 @@
       <h1>Mission Neighbors</h1>
       <p>First and foremost, we hope our Mission neighbors take as much pride in the festival as we do. Amidst parking limitations, we hope our Mission neighbors recognize that the street beyond their front porch is more than just a space for serving delicious food but a platform for supporting food entrepreneurs, many of whom hail from <a href="http://lacocinasf.org/">La Cocina&rsquo;s kitchen</a> and the neighborhood. In the last couple of years, in collaboration with the <a href="http://www.lower24thstreet.org/english/index.html">Lower 24th St. Merchant&rsquo;s Association</a>, we&rsquo;ve been thrilled to see an increase in representation from the commercial corridor in this neighborhood.</p>
       <p>Of course, we always want to hear your thoughts on the festival and we&rsquo;re happy to work with you and help however we can. For any and all questions or just to find out more about <a href="http://www.lacocinasf.org/">La Cocina</a>, <a href="mailto:michellef@lacocinasf.org">email Michelle</a> for more info.</p>
+
+      <p>
+        <img src="/z_photos/2013/mission_neighbors-friends.jpg" alt="Five friends sitting on the sidewalk eating street food." width="535" height="321"/>
+      </p>
     </div>
   </section>
 <?php require 'inc/bottom.inc.php'; ?>
