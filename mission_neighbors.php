44e439d53a7783454cdde8b249802ea6231ce01d
Tiff Ting <tiff@bazaarvoice.com>
2014-04-21 04:47:14 -0500

    Got latest version of Mission Neighbors page

--- mission_neighbors.php
+++ mission_neighbors.php
@@ -7,11 +7,13 @@
     <div class="inner">
       <h1>Mission Neighbors</h1>
       <p>First and foremost, we hope our Mission neighbors take as much pride in the festival as we do. Amidst parking limitations, we hope our Mission neighbors recognize that the street beyond their front porch is more than just a space for serving delicious food but a platform for supporting food entrepreneurs, many of whom hail from <a href="http://lacocinasf.org/">La Cocina&rsquo;s kitchen</a> and the neighborhood. In the last couple of years, in collaboration with the <a href="http://www.lower24thstreet.org/english/index.html">Lower 24th St. Merchant&rsquo;s Association</a>, we&rsquo;ve been thrilled to see an increase in representation from the commercial corridor in this neighborhood.</p>
-      <p>Of course, we always want to hear your thoughts on the festival and we&rsquo;re happy to work with you and help however we can. For any and all questions or just to find out more about <a href="http://www.lacocinasf.org/">La Cocina</a>, <a href="mailto:michellef@lacocinasf.org">email Michelle</a> for more info.</p>
+      <p>Of course, we always want to hear your thoughts on the festival and we&rsquo;re happy to work with you and help however we can. For any and all questions or just to find out more about <a href="http://www.lacocinasf.org/">La Cocina</a>, <a href="mailto:proevent@lacocinasf.org">email Cindy</a> or call <a class="tel" href="tel:4157014683">(415) 701-4683</a> for more info.</p>
 
       <p>
         <img src="/z_photos/2013/mission_neighbors-friends.jpg" alt="Five friends sitting on the sidewalk eating street food." width="535" height="321"/>
       </p>
+
+      <p>To sign up for parking for the 2014 festival, please <a href="https://docs.google.com/forms/d/1Q1XOATz96Rqnj2kZzQjxh0laA9W3NeKsInQqMzu8OpE/viewform">fill out the form here</a> no later than June 2nd</p>
     </div>
   </section>
 <?php require 'inc/bottom.inc.php'; ?>
