3c817523dd1fde1f7cfb3bb61eb71fdcaf6753ca
Tiff Ting <tiff@bazaarvoice.com>
2014-06-09 21:42:56 -0500

    Updated get involved page for 2014

--- get_involved.php
+++ get_involved.php
@@ -6,11 +6,10 @@
   <section class="kraft">
     <div class="inner">
       <h1>Get involved</h1>
-      <p>A huge thank you to the hundreds of volunteers who came to support the 2013 Street Food Festival.  You built seating for tens of thousands of people, shuttled food and ice across perilous crowds of people, sold an unprecedented amount of street food merchandise, and, man, do you know how to rock neon colored t-shirts.</p>
+      <p>We love our volunteers, and we know that we would be lost without them. Join us for an amazing day of all things street food, from tacos to takoyaki, and work one-on-one with the food vendors who make the Bay Area a delicious place year round. We have so many different ways to get involved, and so many neon colored t-shirts for you to rock, and food and drink tokens to quench your cravings once your shift is over. <a href="https://docs.google.com/a/lacocinasf.org/forms/d/1wjE9LY5L8mzKnBayDSq79HpGW9vyZv08-smI1tlfVhc/viewform">Sign up</a> to be a part of the best festival in the Bay!</p>
       <p>
         <img src="/z_photos/2013/get_involved-volunteers.jpg" alt="SFSFF volunteers" width="229" height="343"/>
       </p>
-      <p>Thank you for your support and can&rsquo;t wait to have you at next year&rsquo;s event! Till then, check out La Cocina&rsquo;s <a href="http://lacocinasf.org/volunteer">volunteer page</a> for food and entrepreneurship opportunities year-round.</p>
     </div>
   </section>
 <?php require 'inc/bottom.inc.php'; ?>
