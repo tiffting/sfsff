ba1e7044b8a3efe6592c707a7badd4ae5afc2da4
Tiff Ting <tiff@bazaarvoice.com>
2014-06-09 23:28:19 -0500

    Updated press kit

--- press_kit.php
+++ press_kit.php
@@ -13,25 +13,33 @@
     <div class="inner">
 
       <h1>Press Kit Files</h1>
+
+      <p>For any and all press inquiries or to attend our festival preview dinner on Wednesday, July 9, 2014, contact Michelle at <a href="mailto:michelle@lacocinasf.org">michelle@lacocinasf.org</a>, or call <a href="tel:+14156589205">(415) 658-9205</a> for more information.</p>
+
+      <ul class="press_list">
+        <li><a href="/z_downloads/2014_sfsff_press_release_es.pdf">Festival Press Release Spanish (PDF, 3MB)</a></li>
+        <li><a href="/z_downloads/2014_sfsff_press_release_en.pdf">Festival Press Release (PDF, 3MB)</a></li>
+      </ul>
       
+      <!--
       <h2>2013</h2>
       <ul class="press_list">
-        <li><a href="downloads/2013_sfsff_fact_sheet.pdf">Fact Sheet (.pdf)</a></li>
-        <li><a href="downloads/2013_sfsff_press_release_es.pdf">Festival Press Release Spanish (.pdf)</a></li>
-        <li><a href="downloads/2013_sfsff_press_release_en.pdf">Festival Press Release (.pdf)</a></li>
+        <li><a href="/z_downloads/2013_sfsff_fact_sheet.pdf">Fact Sheet (PDF)</a></li>
+        <li><a href="/z_downloads/2013_sfsff_press_release_es.pdf">Festival Press Release Spanish (PDF)</a></li>
+        <li><a href="/z_downloads/2013_sfsff_press_release_en.pdf">Festival Press Release (PDF)</a></li>
       </ul>
 
       <h2>2012</h2>
       <ul class="press_list">
-        <li><a href="downloads/2012_sfsff_fact_sheet.pdf">Fact Sheet (.pdf)</a></li>
-        <li><a href="downloads/2012_sfsff_press_release_es.pdf">Festival Press Release Spanish (.pdf)</a></li>
-        <li><a href="downloads/2012_sfsff_press_release_en.pdf">Festival Press Release (.pdf)</a></li>
+        <li><a href="/z_downloads/2012_sfsff_fact_sheet.pdf">Fact Sheet (PDF)</a></li>
+        <li><a href="/z_downloads/2012_sfsff_press_release_es.pdf">Festival Press Release Spanish (PDF)</a></li>
+        <li><a href="/z_downloads/2012_sfsff_press_release_en.pdf">Festival Press Release (PDF)</a></li>
       </ul>
 
 	    <h2>2011</h2>
       <ul class="press_list">
-        <li><a href="downloads/2011_sfsff_press_release_en.docx">Festival Press Release (.docx)</a></li>
-        <li><a href="downloads/2011_sfsff_fact_sheet.docx">Festival Fact Sheet (.docx)</a></li>
+        <li><a href="/z_downloads/2011_sfsff_press_release_en.docx">Festival Press Release (Word document)</a></li>
+        <li><a href="/z_downloads/2011_sfsff_fact_sheet.docx">Festival Fact Sheet (Word document)</a></li>
         <li><a href="http://www.lacocinasf.org/representatives-available-for-interview/">Representatives available for Interview</a></li>
         <li><a href="http://www.lacocinasf.org/directory/">Our Entrepreneurs</a></li>
         <li><a href="http://www.lacocinasf.org/lacocinafacsheet/">La Cocina Fact Sheet</a></li>
@@ -39,12 +47,12 @@
       
       <h2>2010</h2>
       <ul class="press_list">
-        <li><a href="downloads/2010_sfsff_scavenger_hunt_release.docx">Scavenger Hunt Press Release (.docx)</a></li>
-        <li><a href="downloads/2010_sfsff_press_release_en.doc">La Cocina Press Release (.doc)</a></li>
-        <li><a href="downloads/2010_sfsff_cart_crawl_release.doc">Cart Crawl Press Release (.doc)</a></li>
+        <li><a href="/z_downloads/2010_sfsff_scavenger_hunt_release.docx">Scavenger Hunt Press Release (Word document)</a></li>
+        <li><a href="/z_downloads/2010_sfsff_press_release_en.doc">La Cocina Press Release (Word document)</a></li>
+        <li><a href="/z_downloads/2010_sfsff_cart_crawl_release.doc">Cart Crawl Press Release (Word document)</a></li>
         <li><a href="http://www.sfstreetfoodfest.com/2010/news-room/">2010 Festival Newsroom Page</a></li>
       </ul>
-      
+      -->
     </div>
   </section>
 <?php require 'inc/bottom.inc.php'; ?>
