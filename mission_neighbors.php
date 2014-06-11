2d9fd18c05d32ea42a99e477bf5322472ac5f7f8
Tiff Ting <tiff@bazaarvoice.com>
2014-06-10 23:58:32 -0500

    Mission neighbors: made parking sign-ups more obvious

--- mission_neighbors.php
+++ mission_neighbors.php
@@ -4,6 +4,11 @@
   require 'inc/top.inc.php';
 ?>
   <section class="kraft">
+    <div class="extra_menu paper">
+      <h3>Parking sign-ups</h3>
+      <p>Please <a href="https://docs.google.com/forms/d/1Q1XOATz96Rqnj2kZzQjxh0laA9W3NeKsInQqMzu8OpE/viewform">fill out this form</a> no later than July 1, 2014 to reserve your parking space during the festival.</p>
+    </div>
+
     <div class="inner">
       <h1>Mission Neighbors</h1>
       <p>First and foremost, we hope our Mission neighbors take as much pride in the festival as we do. Amidst parking limitations, we hope our Mission neighbors recognize that the street beyond their front porch is more than just a space for serving delicious food but a platform for supporting food entrepreneurs, many of whom hail from <a href="http://lacocinasf.org/">La Cocina&rsquo;s kitchen</a> and the neighborhood. In the last couple of years, in collaboration with the <a href="http://www.lower24thstreet.org/english/index.html">Lower 24th St. Merchant&rsquo;s Association</a>, we&rsquo;ve been thrilled to see an increase in representation from the commercial corridor in this neighborhood.</p>
@@ -13,7 +18,7 @@
         <img src="/z_photos/2013/mission_neighbors-friends.jpg" alt="Five friends sitting on the sidewalk eating street food." width="535" height="321"/>
       </p>
 
-      <p>To sign up for parking for the 2014 festival, please <a href="https://docs.google.com/forms/d/1Q1XOATz96Rqnj2kZzQjxh0laA9W3NeKsInQqMzu8OpE/viewform">fill out the form here</a> no later than July 1, 2014.</p>
+      <p>To sign up for parking for the 2014 festival, please <strong><a href="https://docs.google.com/forms/d/1Q1XOATz96Rqnj2kZzQjxh0laA9W3NeKsInQqMzu8OpE/viewform">fill out this form</a></strong> no later than July 1, 2014.</p>
     </div>
   </section>
 <?php require 'inc/bottom.inc.php'; ?>
