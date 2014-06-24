a84c23ec71ad9439157f053ff6ec91540f685265
Tiff Ting <tiff@bazaarvoice.com>
2014-06-23 20:20:39 -0500

    Get involved: updated copy

--- get_involved.php
+++ get_involved.php
@@ -6,10 +6,22 @@
   <section class="kraft">
     <div class="inner">
       <h1>Get involved</h1>
-      <p>We love our volunteers, and we know that we would be lost without them. Join us for an amazing day of all things street food, from tacos to takoyaki, and work one-on-one with the food vendors who make the Bay Area a delicious place year round. We have so many different ways to get involved, and so many neon colored t-shirts for you to rock, and food and drink tokens to quench your cravings once your shift is over. <a href="https://docs.google.com/a/lacocinasf.org/forms/d/1wjE9LY5L8mzKnBayDSq79HpGW9vyZv08-smI1tlfVhc/viewform">Sign up</a> to be a part of the best festival in the Bay!</p>
+      <p>The 6th annual San Francisco Street Food Festival needs you! Help make history, and come join us for the last one on Folsom Street.</p>
+
       <p>
         <img src="/z_photos/2013/get_involved-volunteers.jpg" alt="SFSFF volunteers" width="229" height="343"/>
       </p>
+
+      <p>Please review this list of volunteer position descriptions before filling out the <a href="https://www.surveymonkey.com/s/2014SFSFFVolRegistration">registration form</a>.</p>
+
+      <ul class="text">
+        <li><strong>Pre-festival set-up</strong>: Wear your grubbies and help build the festival! We start Friday afternoon and again early Saturday morning. You get to work with other volunteers, festival staff, and concessionaires setting up blockes, bars, signage, donation stations, and more. This job may involve lifting (40+ lbs) and carrying.</li>
+        <li><strong>People first</strong>: Bring your smile and welcoming nature to the festival! The following positions involve heavy interaction with the public in a very fast-paced environment greeting people, providing guidance, moving festival-goers forward, etc. Physical activity level is medium, and you are mostly on your feet. Opportunities include information booth, passport &amp; VIP sales, sponsor booth, merchandise booth, and VIP brunch support.</li>
+        <li><strong>21 &amp; over</strong> and enjoy working around alcohol? Working around the bar area is great fun for 21 and over volunteers! High energy and medium to high physical activity level.</li>
+        <li><strong>General festival support</strong> (block support), parking &amp; volunteer area: Help our volunteers, concessionaires, and festival participants have a great experience! We need friendly, patient, caring individuals to help support our volunteers and festival staff so they can provide the best experience they can for everyone. This position may include filling in for other volunteer positions when and where needed. Activity level rangs from low to medium.</li>
+      </ul>
+
+      <p class="callout"><a href="https://www.surveymonkey.com/s/2014SFSFFVolRegistration">Volunteer at the festival!</a></p>
     </div>
   </section>
 <?php require 'inc/bottom.inc.php'; ?>
