9c35f688e26c45e0e06197c8933048ef1dce5848
Tiff Ting <tiff@bazaarvoice.com>
2014-07-12 01:32:33 -0500

    Friday Night: added callout

--- friday_night.php
+++ friday_night.php
@@ -4,23 +4,30 @@
   require 'inc/top.inc.php';
 ?>
 <section class="kraft">
+  <div class="extra_menu paper">
+    <h3>Tickets</h3>
+    <p><a href="https://fridaynightfamilymeal.eventbrite.com/">Get your tickets</a> for the Friday Night Family Meal!</p>
+  </div>
+
 	<div class="inner">
+		<section id="intro">
+	    <p class="callout"><a href="https://fridaynightfamilymeal.eventbrite.com/">Buy your tickets today!</a></p>
 
-		<h1>Friday Night Family Meal</h1>
-		<p>That&rsquo;s right, the San Francisco Street Food Festival has a new event!</p>
-		<p>We loved our Night Market, the kick-off event before the Street Food Festival we&rsquo;ve held the last couple of years, but in honor of our last year on Folsom Street, we wanted to try something new, crispy, and something that would be so epic it would quite seriously fry your brain with awesomeness.</p>
-		<p>The answer? Unlimited iterations of fried chicken and open bar (cocktails AND craft beer). Yeah, whoa, we know.</p>
-		<p>As you walk in, you will be (aggressively) greeted with a basket and a full, completely re-fillable flask. There&rsquo;s no time to waste. You take your basket to the nearest chef stations and start filling your basket (aggressively) with varieties from more expected chefs than you know what to do with:</p>
-		<ul class="text">
-			<li>Seattle-based Skillet Street Food (they may even bring their famous bacon jam!)</li>
-			<li>Austin-based food trucks The Peached Tortilla (in their words, they want to get you "peached")</li>
-			<li>Portland-based Big-Ass Sandwiches (with some big-ass flavors of course)</li>
-			<li>Alabama-based Jim n' Nick's (they like it quick n' crisp)</li>
-			<li>Many, many, many more bringing their own styles, twists, tricks, and more...</li>
-		</ul>
-		<p>Yes, it&rsquo;s a bone-dripping, crispy-crunchy fried chicken celebration with some friends who have made the San Francisco Street Food Festival what it is over the years and done it really, really, really deliciously: think past vendors and first-time vendors from around the country coming together to celebrate the end of a Bay Area institution and to support La Cocina, the non-profit organization that hosts the festival every year. Not to mention, the sides (oh, the sides!) made by the bastions of BBQ themselves: The Fatback Collective and Jim 'n Nick's BBQ. And yes, don't fret, we do have vegetarian options, too, and we're not just talking about the open bar from Campari and Rye on the Road. When your hand starts to cramp from tearing into your chicken a little too much, sit back and enjoy the live music and games on-site.</p>
-		<p>All proceeds from the evening benefit La Cocina, the event organizer and a kitchen incubator for low-income immigrant women food entrepreneurs. <a href="https://fridaynightfamilymeal.eventbrite.com/">Donating and paying for a higher ticket price</a> will help open small food businesses, build jobs, and make dreams come true! Thanks in advance!</p>
-
+			<h1>Friday Night Family Meal</h1>
+			<p>That&rsquo;s right, the San Francisco Street Food Festival has a new event!</p>
+			<p>We loved our Night Market, the kick-off event before the Street Food Festival we&rsquo;ve held the last couple of years, but in honor of our last year on Folsom Street, we wanted to try something new, crispy, and something that would be so epic it would quite seriously fry your brain with awesomeness.</p>
+			<p>The answer? Unlimited iterations of fried chicken and open bar (cocktails AND craft beer). Yeah, whoa, we know.</p>
+			<p>As you walk in, you will be (aggressively) greeted with a basket and a full, completely re-fillable flask. There&rsquo;s no time to waste. You take your basket to the nearest chef stations and start filling your basket (aggressively) with varieties from more expected chefs than you know what to do with:</p>
+			<ul class="text">
+				<li>Seattle-based Skillet Street Food (they may even bring their famous bacon jam!)</li>
+				<li>Austin-based food trucks The Peached Tortilla (in their words, they want to get you &ldquo;peached&rdquo;)</li>
+				<li>Portland-based Big-Ass Sandwiches (with some big-ass flavors of course)</li>
+				<li>Alabama-based Jim &rsquo;n Nick&rsquo;s (they like it quick &rsquo;n&rsquo; crisp)</li>
+				<li>Many, many, many more bringing their own styles, twists, tricks, and more...</li>
+			</ul>
+			<p>Yes, it&rsquo;s a bone-dripping, crispy-crunchy fried chicken celebration with some friends who have made the San Francisco Street Food Festival what it is over the years and done it really, really, really deliciously: think past vendors and first-time vendors from around the country coming together to celebrate the end of a Bay Area institution and to support La Cocina, the non-profit organization that hosts the festival every year. Not to mention, the sides (oh, the sides!) made by the bastions of BBQ themselves: The Fatback Collective and Jim &rsquo;n Nick&rsquo;s BBQ. And yes, don&rsquo;t fret, we do have vegetarian options, too, and we&rsquo;re not just talking about the open bar from Campari and Rye on the Road. When your hand starts to cramp from tearing into your chicken a little too much, sit back and enjoy the live music and games on-site.</p>
+			<p>All proceeds from the evening benefit La Cocina, the event organizer and a kitchen incubator for low-income immigrant women food entrepreneurs. <a href="https://fridaynightfamilymeal.eventbrite.com/">Donating and paying for a higher ticket price</a> will help open small food businesses, build jobs, and make dreams come true! Thanks in advance!</p>
+		</section>
 	</div>
 </section>
 <?php require 'inc/bottom.inc.php'; ?>
