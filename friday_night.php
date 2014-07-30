0dfcd560a9c930d9abd65ae9f6a4c6dbd57102b2
Tiff Ting <tiff@bazaarvoice.com>
2014-07-29 22:34:57 -0500

    Friday night: synced content with Eventbrite

--- friday_night.php
+++ friday_night.php
@@ -14,19 +14,64 @@
 	    <p class="callout"><a href="https://fridaynightfamilymeal.eventbrite.com/">Buy your tickets today!</a></p>
 
 			<h1>Friday Night Family Meal</h1>
-			<p>That&rsquo;s right, the San Francisco Street Food Festival has a new event!</p>
-			<p>We loved our Night Market, the kick-off event before the Street Food Festival we&rsquo;ve held the last couple of years, but in honor of our last year on Folsom Street, we wanted to try something new, crispy, and something that would be so epic it would quite seriously fry your brain with awesomeness.</p>
-			<p>The answer? Unlimited iterations of fried chicken and open bar (cocktails AND craft beer). Yeah, whoa, we know.</p>
+			<p><strong>That&rsquo;s right, the San Francisco Street Food Festival has a new event!</strong></p>
+			<p>We loved our Night Market, the kick-off event before the Street Food Festival we&rsquo;ve held the last couple of years, but in honor of our last year on Folsom Street, we wanted to try something new, <em>crispy</em>, and something that would be so epic it would quite seriously fry your brain with awesomeness.</p>
+			<p><strong>The answer?</strong> Unlimited iterations of fried chicken and open bar* (cocktails AND craft beer). Yeah, whoa, we know.</p>
 			<p>As you walk in, you will be (aggressively) greeted with a basket and a full, completely re-fillable flask. There&rsquo;s no time to waste. You take your basket to the nearest chef stations and start filling your basket (aggressively) with varieties from more expected chefs than you know what to do with:</p>
 			<ul class="text">
-				<li>Seattle-based Skillet Street Food (they may even bring their famous bacon jam!)</li>
-				<li>Austin-based food trucks The Peached Tortilla (in their words, they want to get you &ldquo;peached&rdquo;)</li>
-				<li>Portland-based Big-Ass Sandwiches (with some big-ass flavors of course)</li>
-				<li>Alabama-based Jim &rsquo;n Nick&rsquo;s (they like it quick &rsquo;n&rsquo; crisp)</li>
-				<li>Many, many, many more bringing their own styles, twists, tricks, and more...</li>
+				<li>Seattle-based <a href="http://skilletstreetfood.com/">Skillet Street Food</a>: They may even bring their famous bacon jam!</li>
+				<li>Austin-based food trucks <a href="http://thepeachedtortilla.com/">The Peached Tortilla</a>: In their words, they want to get you &ldquo;peached&rdquo;!</li>
+				<li>Alabama-based <a href="http://www.jimnnicks.com/">Jim &rsquo;n Nick&rsquo;s</a>: They like it quick &rsquo;n&rsquo; crisp.</li>
+				<li>Oakland-based <a href="http://hopscotchoakland.com/">Hopscotch</a>: Michael Bauer says the chicken is like <a href="http://www.sfgate.com/restaurants/diningout/article/Hopscotch-an-ambitious-Oakland-place-4082056.php#page-1">&ldquo;a savory granola&rdquo;</a>...in a good way!</li>
+				<li>Sao Paolo, Brazil-based <a href="https://www.facebook.com/FeirinhaGastronomica">Feirinha Gastronomica</a>: Chef Daniela Narciso &amp; Mauricio Shuartz want you to know it&rsquo;ll be boa pra caramba.</li>
+				<li>San Francisco-based Traci des Jardins of <a href="http://www.jardiniere.com/">Jardini&egrave;re</a> and <a href="http://www.thecommissarysf.com/">The Commisary</a>: Chef Traci brings a Top Chef twist to her Top Chef dishes.</li>
+				<li>San Francisco-based <a href="http://tohyang.org/">To Hyang</a>: They&rsquo; coming back from a sudden May 2014 closure to serve their <a href="http://insidescoopsf.sfgate.com/blog/2014/05/05/to-hyang-suddenly-closes-saddening-everyone/">Anthony Bourdain &amp; Chris Consentino</a>-approved fried chicken.
+				<li>San Francisco-based <a href="http://thewholebeast.com/">The Whole Beast</a>: Can&rsquo;t pick between the usual wings and breadsticks? They&rsquo;ve got you covered with the whole beast of a chicken!</li>
+				<li>Oakland-based Japanese fried chicken pop up favorite <a href="https://www.facebook.com/aburayafriedchicken">Abura-Ya</a>: <a href="http://www.eastbayexpress.com/WhatTheFork/archives/2014/06/26/pop-up-alert-abura-ya-brings-japanese-fried-chicken-to-downtown-oakland">The East Bay Express</a> thinks you should try this karaage.</li>
+				<li>San Francisco-based <a href="http://www.starbellysf.com/">Starbelly</a>: Their Fried Chicken event is sold out, but they&rsquo;re coming back with their famous fried chicken for this night only!</li>
+				<li>San Francisco-based (and yet-to-open!) brewery incubator/restaurant <a href="http://sf.eater.com/archives/2014/07/17/citizen_fox_brewery_headed_to_18th_and_mission.php#more">Citizen Fox</a>: Deborah Blum will be giving a preview of her plant-based menu with a vegan &ldquo;Fried Chicken&rdquo; option!</li>
 			</ul>
-			<p>Yes, it&rsquo;s a bone-dripping, crispy-crunchy fried chicken celebration with some friends who have made the San Francisco Street Food Festival what it is over the years and done it really, really, really deliciously: think past vendors and first-time vendors from around the country coming together to celebrate the end of a Bay Area institution and to support La Cocina, the non-profit organization that hosts the festival every year. Not to mention, the sides (oh, the sides!) made by the bastions of BBQ themselves: The Fatback Collective and Jim &rsquo;n Nick&rsquo;s BBQ. And yes, don&rsquo;t fret, we do have vegetarian options, too, and we&rsquo;re not just talking about the open bar from Campari and Rye on the Road. When your hand starts to cramp from tearing into your chicken a little too much, sit back and enjoy the live music and games on-site.</p>
-			<p>All proceeds from the evening benefit La Cocina, the event organizer and a kitchen incubator for low-income immigrant women food entrepreneurs. <a href="https://fridaynightfamilymeal.eventbrite.com/">Donating and paying for a higher ticket price</a> will help open small food businesses, build jobs, and make dreams come true! Thanks in advance!</p>
+			<p>Yes, it&rsquo;s a bone-dripping, crispy-crunchy fried chicken celebration with some friends who have made the San Francisco Street Food Festival what it is over the years and done it really, really, really deliciously: think past vendors and first-time vendors from around the country coming together to celebrate the end of a Bay Area institution and to support <a href="http://www.lacocinasf.org/">La Cocina</a>, the non-profit organization that hosts the festival every year.</p>
+			<p>Not to mention, the sides (oh, the sides!) made by all the chefs above and the bastions of BBQ themselves: <a href="http://thefatbackcollective.tumblr.com/">The Fatback Collective</a> and <a href="http://www.jimnnicks.com/">Jim &rsquo;n Nick&rsquo;s BBQ</a>. And yes, don&rsquo;t fret, we do have vegetarian options, too, and we&rsquo;re not just talking about the <strong>open bar* from <a href="http://calicraft.com/">Calicraft Brewing Co.</a>, <a href="http://www.campari.com/">Campari</a>, and <a href="http://www.ryeontheroad.com/">Rye on the Road</a></strong>. When your hand starts to cramp from tearing into your chicken a little too much, sit back and enjoy the live music and games on-site.</p>
+			<p>Oh yeah, and dessert? Costumed-crusaders <a href="http://threetwinsicecream.com/">Three Twins Ice Cream</a> will be slinging their crazy delicious scoops (a real reason to scream for ice cream) and cookie monster and <a href="http://blogs.kqed.org/checkplease/2013/10/09/copita-tequileria-y-comida-reviews/">man-about-town</a> Omar Mamoon will be bringing his doughpe egg-free <a href="http://doughand.co/">Dough &amp; Co</a> (no more wishful thinking: his cookie dough is edible!)</p>
+			<p>All proceeds from the evening benefit <a href="http://www.lacocinasf.org/">La Cocina</a>, the event organizer and a kitchen incubator for low-income immigrant women food entrepreneurs. <a href="https://fridaynightfamilymeal.eventbrite.com/">Donating and paying for a higher ticket price</a> will help open small food businesses, build jobs, and make dreams come true! Thanks in advance!</p>
+			<p><strong>Sharing is caring</strong> (we agree!), and as much as we can&rsquo;t wait to see all those filtered Instagram shots of you and your friends standing open-mouthed and duck-faced around a heaping bucket o&rsquo; chicken (tag us!: <a href="https://twitter.com/hashtag/sfsff?src=hash">#SFSFF</a>, <a href="https://twitter.com/streetfoodsf">@streetfoodsf</a>), each attendee must purchase an entry ticket at the $50, $75, or $125 range to enter and enjoy said bucket (a real winner winner chicken dinner if we ever heard one). So, as you enjoy that crispy-yet-juicy-ehrmahgerd-piece-of-poultry-perfection, bask in the lustrous glow (er, it might be grease) that comes with supporting your local neighborhood non-profit and local women food-entrepreneurs changing the Bay Area foodscape one tamal, tandoori chicken, and tomato pie at a time.</p>
+			<p><strong>*Heads up, hungry people</strong>: As with any occasion that you should take your passport to (hello, air travel), we&rsquo;re all for pre-gaming the SF Street Food Festival with you long into the night, but open bar with those handcrafted cocktails and craft beer only applies to the $125 level and up. In addition to unlimited servings of fried chicken and sides, the $50 ticket level comes with 1 drink token and the $75 ticket level comes with 2 drink tokens. In case you&rsquo;re feeling extra thirsty after that 2nd helping 6th helping of mac &rsquo;n&rsquo; cheese, or in case Chef Traci des Jardins just challenged you to a fierce Top Chef-themed game of craft beer pong (hey, it could happen), we&rsquo;ll have extra drinks for sale on the night of. Cheers!</p>
+		</section>
+		<section id="sponsor-logos">
+			<h1>Sponsors</h1>
+			<p>Special thanks to the wonderful people who make this event possible including, but not limited to:</p>
+			<div class="sponsor-logos clear">
+        <div class="logo logo-3"><a href="http://www.petalumapoultry.com/"><img src="/z_logos/rocky_chicken.png" alt="Rocky the Range Chicken"></a></div>
+        <div class="logo logo-3"><a href="http://www.petalumapoultry.com/"><img src="/z_logos/rosie_chicken.png" alt="Rosie Organic Chicken"></a></div>
+			</div>
+		</section>
+		<section id="vendor-logos">
+			<h1>Participating chefs &amp; restaurants</h1>
+			<p>The blood, sweat, and grease behind the whole she-bang:</p>
+			<div class="sponsor-logos clear">
+        <div class="logo logo-3"><a href="http://www.jardiniere.com/"><img src="/z_logos/jardiniere.png" alt="Jardini&egrave;re"></a></div>
+        <div class="logo logo-3"><a href="http://www.starbellysf.com/"><img src="/z_logos/starbelly.png" alt="Starbelly"></a></div>
+        <div class="logo logo-3"><a href="http://hopscotchoakland.com/"><img src="/z_logos/hopscotch.png" alt="Hopscotch"></a></div>
+        <div class="logo logo-3"><a href="http://thewholebeast.com/"><img src="/z_logos/the_whole_beast.png" alt="The Whole Beast"></a></div>
+        <div class="logo logo-3"><a href="https://www.facebook.com/aburayafriedchicken"><img src="/z_logos/abura_ya.png" alt="Abura-ya"></a></div>
+        <div class="logo logo-3"><img src="/z_logos/citizen_fox.png" alt="Citizen Fox"></div>
+        <div class="logo logo-3"><a href="http://doughand.co/"><img src="/z_logos/dough_and_co.png" alt="Dough &amp; Co"></a></div>
+        <div class="logo logo-3"><a href="http://www.baiapasta.com/"><img src="/z_logos/baia_pasta.png" alt="Baia Pasta"></a></div>
+        <div class="logo logo-3"><a href="http://threetwinsicecream.com/"><img src="/z_logos/three_twins.png" alt="Three Twins Ice Cream"></a></div>
+        <div class="logo logo-3"><a href="http://tohyang.org/"><img src="/z_logos/to_hyang.gif" alt="To Hyang"></a></div>
+			</div>
+		</section>
+		<section id="drink-logos">
+			<h1>Bourbon, beer, &amp; more</h1>
+			<p>The guys behind your new favorite drinks:</p>
+			<div class="sponsor-logos clear">
+        <div class="logo logo-3"><a href="http://wildturkeybourbon.com/"><img src="/z_logos/wild_turkey.png" alt="Wild Turkey Bourbon"></a></div>
+        <div class="logo logo-3"><a href="http://www.tequilaespolon.com/"><img src="/z_logos/espolon.png" alt="Espol&oacute;n Tequila"></a></div>
+        <div class="logo logo-3"><a href="http://www.appletonestate.com/"><img src="/z_logos/appleton_rum.png" alt="Appleton Estate"></a></div>
+        <div class="logo logo-3"><a href="http://ryeontheroad.com/"><img src="/z_logos/rye_on_the_road.png" alt="Rye on the Road"></a></div>
+        <div class="logo logo-3"><a href="http://www.calicraft.com/"><img src="/z_logos/calicraft.png" alt="Calicraft Brewing"></a></div>
+			</div>
 		</section>
 	</div>
 </section>
