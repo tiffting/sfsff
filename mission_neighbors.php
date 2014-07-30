11269161ee450f9f0ddab4bdbf7a88f72cc77424
Tiff Ting <tiff@bazaarvoice.com>
2014-07-29 21:26:46 -0500

    Mission neighbors: changed contact and removed parking

--- mission_neighbors.php
+++ mission_neighbors.php
@@ -4,15 +4,10 @@
   require 'inc/top.inc.php';
 ?>
   <section class="kraft">
-    <div class="extra_menu paper">
-      <h3>Parking sign-ups</h3>
-      <p>Please <a href="https://docs.google.com/forms/d/1Q1XOATz96Rqnj2kZzQjxh0laA9W3NeKsInQqMzu8OpE/viewform">fill out this form</a> no later than July 1, 2014 to reserve your parking space during the festival.</p>
-    </div>
-
     <div class="inner">
       <h1>Mission Neighbors</h1>
       <p>First and foremost, we hope our Mission neighbors take as much pride in the festival as we do. Amidst parking limitations, we hope our Mission neighbors recognize that the street beyond their front porch is more than just a space for serving delicious food but a platform for supporting food entrepreneurs, many of whom hail from <a href="http://lacocinasf.org/">La Cocina&rsquo;s kitchen</a> and the neighborhood. In the last couple of years, in collaboration with the <a href="http://www.lower24thstreet.org/english/index.html">Lower 24th St. Merchant&rsquo;s Association</a>, we&rsquo;ve been thrilled to see an increase in representation from the commercial corridor in this neighborhood.</p>
-      <p>Of course, we always want to hear your thoughts on the festival and we&rsquo;re happy to work with you and help however we can. For any and all questions or just to find out more about <a href="http://www.lacocinasf.org/">La Cocina</a>, <a href="mailto:proevent@aol.com">email proevent@aol.com</a> for more information.</p>
+      <p>Of course, we always want to hear your thoughts on the festival and we&rsquo;re happy to work with you and help however we can. For any and all questions or just to find out more about <a href="http://www.lacocinasf.org/">La Cocina</a>, email <a href="mailto:proevent@aol.com">proevent@aol.com</a> for more information.</p>
 
       <p>
         <img src="/z_photos/2013/mission_neighbors-friends.jpg" alt="Five friends sitting on the sidewalk eating street food." width="535" height="321"/>
