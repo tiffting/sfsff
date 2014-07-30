329084c3d6d520a426ed1c91f18dfcb2fadf10d6
Tiff Ting <tiff@bazaarvoice.com>
2014-07-29 20:44:08 -0500

    Sponsors: added and removed logos

--- sponsors.php
+++ sponsors.php
@@ -49,7 +49,7 @@
           <!-- <div class="logo logo-3"><a href="http://www.cupcakevineyards.com/"><img src="/z_logos/cupcake_vineyards.png" alt="Cupcake Vineyards"></a></div> -->
           <div class="logo logo-3"><a href="http://petalumapoultry.com/"><img src="/z_logos/petaluma_poultry.png" alt="Petaluma Poultry"></a></div>
           <div class="logo logo-3"><a href="http://offthegridsf.com/"><img src="/z_logos/off_the_grid.png" alt="Off the Grid"></a></div>
-          <div class="logo logo-3"><a href="http://cater2.me/"><img src="/z_logos/cater2me.png" alt="Cater2.me"></a></div>
+          <!-- <div class="logo logo-3"><a href="http://cater2.me/"><img src="/z_logos/cater2me.png" alt="Cater2.me"></a></div> -->
           <!-- <div class="logo logo-3"><a href="http://www.runa.org/"><img src="/z_logos/runa.png" alt="Runa"/></a></div> -->
           <div class="logo logo-3"><a href="http://www.liifund.org/"><img src="/z_logos/liif.png" alt="LIIF"></a></div>
           <div class="logo logo-3"><a href="http://calicraft.com/"><img src="/z_logos/calicraft.png" alt="Calicraft Brewing Co."></a></div>
@@ -66,18 +66,18 @@
           <!-- <div class="logo logo-3"><a href="http://www.goodfoodmerchantsguild.org/"><img src="/z_logos/gfmg.png" alt="Good Food Merchants Guild"></a></div> -->
           <!-- <div class="logo logo-3"><a href="http://www.smartcentersanfrancisco.com/"><img src="/z_logos/smart_car.png" alt="Smart Car San Francisco"></a></div> -->
           <div class="logo logo-3"><a href="http://www.thegraphicsresource.com/"><img src="/z_logos/tgr.png" alt="The Graphics Resource"></a></div>
-          <!-- <div class="logo logo-3"><a href="http://www.thebolditalic.com/"><img src="/z_logos/the_bold_italic.png" alt="The Bold Italic"></a></div> -->
+          <div class="logo logo-3"><a href="http://www.thebolditalic.com/"><img src="/z_logos/the_bold_italic.png" alt="The Bold Italic"></a></div>
           <!-- <div class="logo logo-3"><a href="http://www.sfexaminer.com/"><img src="/z_logos/sf_examiner.png" alt="The San Francisco Examiner"></a></div> -->
           <div class="logo logo-3"><a href="http://www.williams-sonoma.com/"><img src="/z_logos/williams_sonoma.png" alt="Williams-Sonoma"></a></div>
-          <div class="logo logo-3"><a href="http://www.zipcar.com/"><img src="/z_logos/zipcar.png" alt="Zipcar"></a></div>
+          <!-- <div class="logo logo-3"><a href="http://www.zipcar.com/"><img src="/z_logos/zipcar.png" alt="Zipcar"></a></div> -->
           <div class="logo logo-3"><a href="http://www.specialtyfood.com/"><img src="/z_logos/specialty_food.png" alt="Specialty Food Association"></a></div>
           <div class="logo logo-3"><a href="http://www.square.com/"><img src="/z_logos/square.png" alt="Square"></a></div>
-          <!-- <div class="logo logo-3"><a href="http://www.sfweekly.com/"><img src="/z_logos/sf_weekly.gif" alt="SF Weekly"></a></div> -->
+          <div class="logo logo-3"><a href="http://www.sfweekly.com/"><img src="/z_logos/sf_weekly.gif" alt="SF Weekly"></a></div>
           <!-- <div class="logo logo-3"><a href="http://www.plated.com/"><img src="/z_logos/plated.png" alt="Plated"></a></div> -->
           <!-- <div class="logo logo-3"><a href="http://www.tastemade.com/"><img src="/z_logos/tastemade.png" alt="Tastemade"></a></div> -->
           <!-- <div class="logo logo-3"><a href="http://giantagency.com/"><img src="/z_logos/giant.png" alt="Giant Creative/Strategy, LLC"></a></div> -->
           <!-- <div class="logo logo-3"><a href="http://www.sfbar.org/lawyerreferrals/"><img src="/z_logos/sf_bar_referral.png" alt="Bar Association of SF's Lawyer Referral and Info Service"></a></div> -->
-          <!-- <div class="logo logo-3"><a href="http://www.google.com/"><img src="/z_logos/google.png" alt="Google"></a></div> -->
+          <div class="logo logo-3"><a href="http://www.google.com/"><img src="/z_logos/google.png" alt="Google"></a></div>
           <!-- <div class="logo logo-3"><a href="http://www.crunch.com/"><img src="/z_logos/crunch.png" alt="Crunch"></a></div> -->
           <!-- <div class="logo logo-3"><a href="http://chefsleeve.com/"><img src="/z_logos/chef_sleeve.png" alt="Chef Sleeve"></a></div> -->
           <!-- <div class="logo logo-3"><a href="http://eatfeastly.com/"><img src="/z_logos/feastly.png" alt="Feastly"></a></div> -->
@@ -87,24 +87,28 @@
           <!-- <div class="logo logo-3"><a href="http://www.robertvaldesphotography.com/"><img src="/z_logos/robert_valdes.png" alt="Robert Valdes Photo Cinema"></a></div> -->
           <!-- <div class="logo logo-3"><a href="http://strausfamilycreamery.com/"><img src="/z_logos/straus.png" alt="Straus Family Creamery"></a></div> -->
           <!-- <div class="logo logo-3"><a href="http://tecate.com/"><img src="/z_logos/tecate.png" alt="Tecate"></a></div> -->
-          <!-- <div class="logo logo-3"><a href="http://www.heineken.com/"><img src="/z_logos/heineken.png" alt="Heineken"></a></div> -->
-          <!-- <div class="logo logo-3"><a href="http://strongbowcider.com/"><img src="/z_logos/strongbow.png" alt="Strongbow Cider"></a></div> -->
-          <!-- <div class="logo logo-3"><a href="https://www.facebook.com/newcastle"><img src="/z_logos/newcastle.png" alt="Newcastle Brown Ale"></a></div> -->
-          <!-- <div class="logo logo-3"><a href="http://amstellight.com/"><img src="/z_logos/amstel_light.png" alt="Amstel Light"></a></div> -->
+          <div class="logo logo-3"><a href="http://www.heineken.com/"><img src="/z_logos/heineken.png" alt="Heineken"></a></div>
+          <div class="logo logo-3"><a href="http://strongbowcider.com/"><img src="/z_logos/strongbow.png" alt="Strongbow Cider"></a></div>
+          <div class="logo logo-3"><a href="https://www.facebook.com/newcastle"><img src="/z_logos/newcastle.png" alt="Newcastle Brown Ale"></a></div>
+          <div class="logo logo-3"><a href="http://amstellight.com/"><img src="/z_logos/amstel_light.png" alt="Amstel Light"></a></div>
           <!-- <div class="logo logo-3"><a href="http://americantripps.com/"><img src="/z_logos/american_tripps.png" alt="American Tripps"></a></div> -->
           <!-- <div class="logo logo-3"><a href="http://www.kingshawaiian.com/"><img src="/z_logos/kings_hawaiian.png" alt="King&rsquo;s Hawaiian"></a></div> -->
-          <!-- <div class="logo logo-3"><a href="http://www.mahatmarice.com/"><img src="/z_logos/mahatma.gif" alt="Mahatma Rice"></a></div> -->
-          <!-- <div class="logo logo-3"><a href="http://www.oaklandpallet.com/"><img src="/z_logos/oakland_pallet.gif" alt="Oakland Pallet Company"></a></div> -->
+          <div class="logo logo-3"><a href="http://www.mahatmarice.com/"><img src="/z_logos/mahatma.gif" alt="Mahatma Rice"></a></div>
+          <div class="logo logo-3"><a href="http://www.oaklandpallet.com/"><img src="/z_logos/oakland_pallet.gif" alt="Oakland Pallet Company"></a></div>
           <!-- <div class="logo logo-3"><a href="http://www.absseafood.com/"><img src="/z_logos/abs.png" alt="ABS Seafood"></a></div> -->
           <!-- <div class="logo logo-3"><a href="http://zynga.org/"><img src="/z_logos/zynga_org.png" alt="Zynga.org"></a></div> -->
           <!-- <div class="logo logo-3"><a href="http://www.emighsmarket.com/"><img src="/z_logos/emigh_lamb.png" alt="Emigh Lamb"></a></div> -->
           <!-- <div class="logo logo-3"><a href="http://www.stonevalleyfarm.com/"><img src="/z_logos/stone_valley.gif" alt="Stone Valley Farm"></a></div> -->
-          <!-- <div class="logo logo-3"><a href="http://www.snapfiesta.com/"><img src="/z_logos/snapfiesta.png" alt="SnapFiesta"></a></div> -->
+          <div class="logo logo-3"><a href="http://www.snapfiesta.com/"><img src="/z_logos/snapfiesta.png" alt="SnapFiesta"></a></div>
           <!-- <div class="logo logo-3"><a href="http://www.trumer-international.com/"><img src="/z_logos/trumer_pils.png" alt="Trumer Pils"></a></div> -->
           <!-- <div class="logo logo-3"><a href="http://www.capitalone.com/"><img src="/z_logos/capital_one.png" alt="Capital One"></a></div> -->
           <!-- <div class="logo logo-3"><a href="http://www.alitedesigns.com/"><img src="/z_logos/alite.png" alt="Alite Designs"></a></div> -->
-          <!-- <div class="logo logo-3"><a href="http://sfrecpark.org/"><img src="/z_logos/sf_rec_park.png" alt="San Francisco Recreation and Parks"></a></div> -->
+          <div class="logo logo-3"><a href="http://sfrecpark.org/"><img src="/z_logos/sf_rec_park.png" alt="San Francisco Recreation and Parks"></a></div>
           <!-- <div class="logo logo-3"><a href="http://www.instantcab.com/"><img src="/z_logos/instantcab.png" alt="InstantCab"></a></div> -->
+          <div class="logo logo-3"><a href="http://www.clearlykombucha.com/"><img src="/z_logos/clearly_kombucha.png" alt="Clearly Kombucha"></a></div>
+          <div class="logo logo-3"><a href="http://www.pedrosmovingservices.com/"><img src="/z_logos/pedros_moving_services.png" alt="Pedro's Moving Services"></a></div>
+          <div class="logo logo-3"><a href="http://www.tonysfinefoods.com/"><img src="/z_logos/tonys.png" alt="Tony's Fine Foods"></a></div>
+          <div class="logo logo-3"><a href="https://www.wellsfargo.com/"><img src="/z_logos/wells_fargo.png" alt="Wells Fargo"></a></div>
           <!--
           <div class="logo logo-3"><a href=""><img src="/z_logos/" alt=""></a></div>
           -->
