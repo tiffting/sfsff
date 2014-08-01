a55ceb38d5e673d9c1e64af23b9884c7550882bf
Tiff Ting <tiff@bazaarvoice.com>
2014-08-01 01:28:11 -0500

    Home: added raffle

--- inc/nav.inc.php
+++ inc/nav.inc.php
@@ -1,19 +1,37 @@
 <ul>
-  <li class="get_list_ready"><a id="nav_list" href="list.php">Get Your List Ready</a></li>
-  <li class="press_kit"><a href="press_kit.php">Press Kit</a></li>
-  <li class="<?= $page_id == "about" ? 'active' : '' ?>"><a id="nav_about" href="about.php">About</a></li>
-  <li class="<?= $page_id == "eat_and_drink" ? 'active' : '' ?>"><a id="nav_eat_and_drink" href="eat_and_drink.php">Eat/Drink</a></li>
-  <li class="<?= $page_id == "conference" ? 'active' : '' ?>"><a id="nav_conference" href="conference.php">Conference</a></li>
-  <li class="passports"><a id="nav_passports" href="http://sfstreetfoodfestival2014.eventbrite.com/">Passports</a></li>
-  <li class="<?= $page_id == "news" ? 'active' : '' ?>"><a id="nav_news" href="https://www.facebook.com/sanfranciscostreetfoodfestival">News</a></li>
-  <li class="<?= $page_id == "sponsors" ? 'active' : '' ?>"><a id="nav_sponsors" href="sponsors.php">Sponsors</a></li>
-  <li class="<?= $page_id == "get_involved" ? 'active' : '' ?>"><a id="nav_get_involved" href="get_involved.php">Get Involved</a></li>
-  <?php 
-  /*<li class="<?= $page_id == "night_market" ? 'active' : '' ?>"><a id="nav_night_market" href="night_market.php">Night Market</a></li>*/
-  ?>
-  <li class="friday_night"><a href="friday_night.php">Friday Night Family Meal</a></li>
-  <li class="photo_contest"><a href="app.php" id="nav_photo_contest">Photo Contest</a></li>
-  <li class="mission_neighbors"><a id="mission_neighbors" href="mission_neighbors.php">Mission Neighbors</a></li>
+  <li class="press_kit<?= $page_id == "press_kit" ? ' active' : '' ?>">
+    <a href="press_kit.php">Press Kit</a>
+  </li>
+  <li class="about<?= $page_id == "about" ? ' active' : '' ?>">
+    <a id="nav_about" href="about.php">About</a>
+  </li>
+  <li class="eat_and_drink<?= $page_id == "eat_and_drink" ? ' active' : '' ?>">
+    <a id="nav_eat_and_drink" href="eat_and_drink.php">Eat/Drink</a>
+  </li>
+  <li class="conference<?= $page_id == "conference" ? ' active' : '' ?>">
+    <a id="nav_conference" href="conference.php">Conference</a>
+  </li>
+  <li class="passports">
+    <a id="nav_passports" href="http://sfstreetfoodfestival2014.eventbrite.com/">Passports</a>
+  </li>
+  <li class="news">
+    <a id="nav_news" href="https://www.facebook.com/sanfranciscostreetfoodfestival">News</a>
+  </li>
+  <li class="raffle">
+    <a href="www.lacocinasf.org/?page_id=10617&preview=true">Raffle</a>
+  </li>
+  <li class="sponsors<?= $page_id == "sponsors" ? ' active' : '' ?>">
+    <a id="nav_sponsors" href="sponsors.php">Sponsors</a>
+  </li>
+  <li class="get_involved<?= $page_id == "get_involved" ? ' active' : '' ?>">
+    <a id="nav_get_involved" href="get_involved.php">Get Involved</a>
+  </li>
+  <li class="friday_night<?= $page_id == "friday_night" ? ' active' : '' ?>">
+    <a href="friday_night.php">Friday Night Family Meal</a>
+  </li>
+  <li class="mission_neighbors<?= $page_id == "mission_neighbors" ? ' active' : '' ?>">
+    <a id="mission_neighbors" href="mission_neighbors.php">Mission Neighbors</a>
+  </li>
   <li class="social">
     <a class="twitter" href="http://twitter.com/streetfoodsf"><img src="images/home_nav/t.png" /></a>
     <a class="facebook" href="https://www.facebook.com/pages/San-Francisco-Street-Food-Festival/108586905834574"><img src="images/home_nav/fb.png" /></a>
