9e54db3857ec145ff01f5756c55a8509cabfefa4
Tiff Ting <tiff@bazaarvoice.com>
2014-08-14 08:26:22 -0500

    Home: added link to app

--- inc/bottom.inc.php
+++ inc/bottom.inc.php
@@ -3,15 +3,10 @@
     <footer>
       <img src="images/home_nav/presented_by_wf.png" alt="Presented by Whole Foods!" title="Presented by Whole Foods" width="150" height="57"/>
     </footer>
-    <?php
-    /*
     <div id="app-ribbon">
-	  <a href="https://itunes.apple.com/us/app/sf-street-food-festival/id680018355?ls=1&mt=8" 
-	  	 target="_blank">
-	   <img src="images/sfsff_app_ribbon.png" title="Download The App" /> 
+	  <a href="https://itunes.apple.com/us/app/id680018355">
+      <img src="images/sfsff_app_ribbon.png" title="Download the SF Street Food Festival app!"/>
 	  </a>
 	</div>
-	*/
-	?>
   </body>
 </html>
